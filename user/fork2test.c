93771a5ba3b950cf8a085e4632fd0b0dfb334e1a
Luis Maya Aranda <mayafluis@gmail.com>
2021-09-20 15:56:00 -0700

    Updated fork2test

--- user/fork2test.c
+++ user/fork2test.c
@@ -5,41 +5,46 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define N  500
+#define N 500
 
-void
-print(const char *s)
-{
-  write(1, s, strlen(s));
+void print(const char *s) {
+    write(1, s, strlen(s));
 }
 
 void fork2test(void) {
-  int n, pid = 0 ;
+    int n, pid;
 
-  print("fork2 test\n");
-  for(n = 0; n < N; n++) {
-    pid = fork2(2);
-    if(pid > 0)
-      wait(0);
-    else if(pid == 0)
-      exit(0);
-    else {
-      print("fork2test: FAIL\n");
-      exit(1);
+    print("fork2 test\n");
+
+    for (n = 0; n < N; n++) {
+        pid = fork2(2);
+        if (pid < 0)
+            break;
+        if (pid == 0)
+            exit(0);
     }
-  }
 
-  if(wait(0) != -1) {
-    print("fork2test: FAIL\n");
-    exit(1);
-  }
+    if (n == N) {
+        print("fork2 claimed to work N times!\n");
+        exit(1);
+    }
 
-  print("fork2test: OK\n");
+    for (; n > 0; n--) {
+        if (wait(0) < 0) {
+            print("wait stopped early\n");
+            exit(1);
+        }
+    }
+
+    if (wait(0) != -1) {
+        print("wait got too many\n");
+        exit(1);
+    }
+
+    print("fork2 test OK\n");
 }
 
-int
-main(void)
-{
-  fork2test();
-  exit(0);
+int main(void) {
+    fork2test();
+    exit(0);
 }
